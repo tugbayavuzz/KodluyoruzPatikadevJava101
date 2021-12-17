afb383239cf14f76d7a4883fedbad9593813fb64
tugbayavuzz <tugbayavuz@yandex.com>
2021-12-17 21:43:12 +0300

    taximeter calculated

---
+++ src/com/kodluyoruzpatikadev/TaximeterProgram/TaximeterProgramMain.java
@@ -0,0 +1,22 @@
+package com.kodluyoruzpatikadev.TaximeterProgram;
+
+import java.util.Scanner;
+
+public class TaximeterProgramMain {
+    public static void main(String[] args) {
+
+        int km;
+        double total, perKm = 2.20, startPrice = 10;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the KM: ");
+        km = input.nextInt();
+
+        total = perKm*km;
+        total += startPrice;
+
+        System.out.println("Total price : " +total);
+
+    }
+
+}
