2360724683aa4350fe6ed6ecf89ae937906c8a2a
tugbayavuzz <tugbayavuz@yandex.com>
2021-12-17 22:16:00 +0300

    circle area and perimeter calculated

---
+++ src/com/kodluyoruzpatikadev/CalculateCircleArea/CalculateCircleMain.java
@@ -0,0 +1,22 @@
+package com.kodluyoruzpatikadev.CalculateCircleArea;
+
+import java.util.Scanner;
+
+public class CalculateCircleMain {
+    public static void main(String[] args) {
+         int radius;
+         final double PI = 3.14;
+         double area, perimeter,alpha = 0;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius: ");
+        radius = input.nextInt();
+
+        perimeter = 2*Math.PI*radius;
+        System.out.println("Perimeter of the circle: " +perimeter);
+
+        area = (Math.PI*radius*radius);
+        System.out.println("Area of the circle: " +area);
+
+    }
+}
