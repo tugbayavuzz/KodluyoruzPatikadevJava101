c2c53df6c967ebe14172d7aacc7b8a6235b230e3
tugbayavuzz <tugbayavuz@yandex.com>
2021-12-20 19:23:07 +0300

    loops examples2

--- src/com/kodluyoruzpatikadev/Loops/EvenNumbers.java
+++ src/com/kodluyoruzpatikadev/Loops/EvenNumbers.java
@@ -11,20 +11,22 @@ public class EvenNumbers {
         System.out.println("Enter the number");
         number = input.nextInt();
 
-        /*
+        //Program to Find Even Numbers Up To The Entered Number
         for (int i = 1; i <= number; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
 
         }
-         */
+
+        //Program that calculates the average of numbers divisible by 3 and 4 from numbers from 0 to the number entered.
+        int total = 0, count = 0;
         for (int i = 0; i <= number; i++) {
-            if (i % 3 == 0 && i%4 == 0) {
-                System.out.println(i);
+            if (i % 3 == 0 && i % 4 == 0) {
+                total += i;
+                count++;
             }
-
         }
-
+        System.out.println("Average : " + (total / count));
     }
 }
