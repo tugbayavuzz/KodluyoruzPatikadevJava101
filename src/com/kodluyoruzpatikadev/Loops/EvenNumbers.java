941dfe74e17ef91cbcdedb1eae590cf06a0b5488
tugbayavuzz <tugbayavuz@yandex.com>
2021-12-22 14:48:00 +0300

    power of numbers

--- src/com/kodluyoruzpatikadev/Loops/EvenNumbers.java
+++ src/com/kodluyoruzpatikadev/Loops/EvenNumbers.java
@@ -11,7 +11,6 @@ public class EvenNumbers {
         System.out.println("Enter the number");
         number = input.nextInt();
 
-
         //Program to Find Even Numbers Up To The Entered Number
         for (int i = 1; i <= number; i++) {
             if (i % 2 == 0) {
@@ -19,7 +18,7 @@ public class EvenNumbers {
             }
 
         }
-
+/* ************************************************************************************************** */
         //Program that calculates the average of numbers divisible by 3 and 4 from numbers from 0 to the number entered.
 
         for (int i = 0; i <= number; i++) {
@@ -29,5 +28,26 @@ public class EvenNumbers {
                 System.out.println("Average : " + (total / count));
             }
         }
+
+/* ************************************************************************************************** */
+        //Program to Find Powers of 2 Less Than Entered Number
+        for (int i = 2; i <= number; i*=2){
+            System.out.println(i);
+        }
+
+/* **************************************************************************************************** */
+        //Program that prints powers of 4 and 5 up to the entered number
+        for (int i = 1; i <= number; i *= 4){
+
+            System.out.println(i);
+        }
+
+        System.out.println("*********************");
+
+        for (int j = 1; j <= number; j*=5){
+            System.out.println(j);
+        }
+
+
     }
 }
