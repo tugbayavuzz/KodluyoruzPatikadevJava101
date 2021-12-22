448cb5d682f960ef29968591a7bf8cb6f4a6fa2a
tugbayavuzz <tugbayavuz@yandex.com>
2021-12-22 19:14:45 +0300

    harmonic numbers calculate

---
+++ src/com/kodluyoruzpatikadev/Loops/HarmonicNumbers.java
@@ -0,0 +1,21 @@
+package com.kodluyoruzpatikadev.Loops;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class HarmonicNumbers {
+    public static void main(String[] args) {
+        int n;
+        double total = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the N number");
+        n = input.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            total += (double) 1 / i;
+        }
+        System.out.println(total);
+    }
+}
