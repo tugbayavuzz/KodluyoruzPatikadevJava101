94a04f354e100b5e48bc0b020941f9d0bf02e81c
tugbayavuzz <tugbayavuz@yandex.com>
2021-12-23 21:49:48 +0300

    inverted triangle_2

--- src/com/kodluyoruzpatikadev/Loops/InvertedTriangle.java
+++ src/com/kodluyoruzpatikadev/Loops/InvertedTriangle.java
@@ -10,11 +10,11 @@ public class InvertedTriangle {
         System.out.print("Enter the number : ");
         int n = input.nextInt();
 
-        for (int a = n-1; a >= 0; a--){
-            for (int b = 0; b >= (a - n-1); b--){
+        for (int a = n - 1; a >= 0; a--) {
+            for (int b = 0; b >= (a - n - 1); b--) {
                 System.out.print(" ");
             }
-            for (int c = (2*a-1); c>=1; c--){
+            for (int c = (2 * a - 1); c >= 1; c--) {
                 System.out.print("*");
             }
             System.out.println("");
