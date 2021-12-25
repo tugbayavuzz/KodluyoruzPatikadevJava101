3d32ff0adbb250d89dbb9dd1a0f07b2234c4deff
tugbayavuzz <tugbayavuz@yandex.com>
2021-12-26 00:58:45 +0300

    prime number recursive

---
+++ src/com/kodluyoruzpatikadev/Loops/PrimeRecursive.java
@@ -0,0 +1,29 @@
+package com.kodluyoruzpatikadev.Loops;
+
+import java.util.Scanner;
+
+public class PrimeRecursive {
+    static int  i = 2;
+
+    static boolean isPrime(int n){
+        if (n == 1 || n== 0)
+            return false;
+
+        if (n == i)
+            return true;
+
+        if (n % i == 0)
+        {
+            return false;
+        }
+        i++;
+        return isPrime(n);
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number :");
+        int n= input.nextInt();
+        System.out.println(isPrime(n));
+
+    }
+}
